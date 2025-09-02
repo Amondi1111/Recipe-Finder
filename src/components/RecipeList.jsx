82f490b1dff6ce5f92a20742fc7a8f7afaa7797f
Patricia Otieno <dylanpatricia04@gmail.com>
2025-09-02 15:52:57 +0300

    Created RecipeList.jsx with props, empty states, mapping RecipeCard and LoadingComponent

---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,31 @@
+import RecipeCard from './RecipeCard';
+import LoadingComponent from './LoadingComponent';
+
+function RecipeList({ recipes = [], isLoading }) {
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center py-10">
+                <LoadingComponent />
+            </div>
+        );
+    }
+
+    if (!recipes || recipes.length === 0) {
+        return (
+            <div className="text-center text-gray-500 py-10">
+                <p>Try searching for something else. Recipe not found!</p>
+            </div>
+        );
+
+    }
+    return (
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-col-3 lg:grid-cols-4 p-4">
+            {recipes.map((recipe) => (
+                <RecipeCard key={recipe.id} recipe={recipe} />
+
+            ))}
+        </div>
+    );
+}
+
+export default RecipeList;
