d459c015a70bf31d4421507c8f311909a68a0a85
Patricia Otieno <dylanpatricia04@gmail.com>
2025-09-03 14:32:03 +0300

    Fix typo in method name to .substring instead of .substrings

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -5,25 +5,27 @@ function RecipeCard({ recipe }) {
         <div className="bg-[#FF6347] rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow">
             
             {/*For recipe image */}
-            {recipe.image && (
+            {recipe.strMealThumb && (
                 <img
-                src={recipe.image}
-                alt={recipe.title}
-                className="w-full h-35 object-cover" />
+                src={recipe.strMealThumb}
+                alt={recipe.strMeal}
+                className="w-full h-36 object-cover" />
         
             )}
             
             {/*Details for the image cards */}
             <div className="p-4">
                 <h2 className="text-lg font-bold text-gray-700 truncate">
-                    {recipe.title}
+                    {recipe.strMeal}
                 </h2>
                 <p className="text-sm text-gray-600 my-2 line-clamp-2">
-                    {recipe.description || "No description is available now."}
+                    {recipe.strInstructions
+                    ? recipe.strInstructions.substring(0, 120) + "..."
+                    : "No description is available now."}
                 </p>
 
                 {/*For button */}
-                <button className="mt-4 w-full bg-[#FF6347] text-whitenpy-2 rounded-md hover:bg-[#9B3131] transition">
+                <button className="mt-4 w-full bg-[#FF6347] text-whiten py-2 rounded-md hover:bg-[#9B3131] transition">
                    View Recipe 
                 </button>
             </div>
